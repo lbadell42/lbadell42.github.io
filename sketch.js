82e7c0412a4cd1de99d9914780beea8243fe3375
lbadell42 <127773962+lbadell42@users.noreply.github.com>
2023-03-15 09:55:53 -0400

    Initial commit
    finished project

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// this is for your game/web toy
+var paddel1;
+var paddel1Position;
+
+function preload() {
+    paddel1 = loadImage('file:///Users/liambadell/Desktop/Screen%20Shot%202023-02-28%20at%203.32.31%20PM.png');
+  }
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    paddel1Position = 200;
+}
+
+function draw() {
+    
+}
